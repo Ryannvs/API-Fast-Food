f6fd316241744936746e09753cdb0e50e18dcd26
Ryannvs <ryannvs89@gmail.com>
2025-09-03 10:51:08 -0300

    Feito os controllers, faltando apenas o atualizar e deletar

--- src/controller/clientController.js
+++ src/controller/clientController.js
@@ -5,11 +5,11 @@ const clientModel = require('../model/clientModel');
 //Função que executa o método da camada model 
 const getAllClients = (req, res) => {
     const client = clientModel.findAll();
-    //se der certo transforme o user em json
+    //se der certo transforme o client em json
     res.status(200).json(client);
 }
 
-// Método do controlador para obter um usuario por id
+// Método do controlador para obter um client por id
 const getClientById = (req, res) => {
 
     // Pegando o id que foi enviado na requisição 
@@ -18,29 +18,53 @@ const getClientById = (req, res) => {
     // req.params.id | pegue o parametro id 
     const id = parseInt(req.params.id);
     
-    //Chamando o método findById do userModel
+    //Chamando o método findById do clientModel
     const client = clientModel.findById(id);
 
     if (client) {
         //Responder com status code de 200 (Sucesso!)
-        // e devolver os dados do usuario em formato json
+        // e devolver os dados do cliente em formato json
         res.status(200).json(client);
         //se não tiver sucesso
     } else {
         //devolver um 
-        res.status(404).json({ mensagem: 'Usuário não encontrado no banco de dados!' });
+        res.status(404).json({ mensagem: 'Cliente não encontrado no banco de dados!' });
+    }
+};
+// Método do controlador para obter um client por id
+const getClientByName = (req, res) => {
+
+    // Pegando o id que foi enviado na requisição 
+    // id sendo passado pelo req, parametro que manipula os dados que veio pela requisição
+    // res é a resposta da requisição 
+    // req.params.id | pegue o parametro id 
+    const name = String (req.params.name);
+    
+    //Chamando o método findById do clientModel
+    const client = clientModel.findByName(name);
+
+    if (client) {
+        //Responder com status code de 200 (Sucesso!)
+        // e devolver os dados do cliente em formato json
+        res.status(200).json(client);
+        //se não tiver sucesso
+    } else {
+        //devolver um 
+        res.status(404).json({ mensagem: 'Cliente não encontrado no banco de dados!' });
     }
 };
 
-//Método do controlador para criar um novo usuario
+//Método do controlador para criar um novo client
 const createClient = (req, res) => {
     // Pegando os dados que foram enviador pelo Body da requisição
     //Desestruturação, pegando algo estruturado e guardo em variáveis
     const {name, email, telefone, endereco, dataCadastro} = req.body;
 
-    //validar se foram enviados
+    //validar se foram enviados senão retorna mensagem de erro
     if(!name || !email || !telefone || !endereco || !dataCadastro) {
         return res.status(400).json({mensagem: "Nome, email, telefone, endereço e data de cadastro são campos obrigatórios!"});
+    
+    //Se estiver tudo correto, retornará a mensagem 201 que é de sucesso
     } else {
         const newClient = clientModel.create({name, email, telefone, endereco, dataCadastro});
         res.status(201).json(newClient);
@@ -48,10 +72,11 @@ const createClient = (req, res) => {
 }
 
 
-//exportando o metodo getAllUsers
+//exportando os métodos
 module.exports = {
     getAllClients,
     getClientById,
+    getClientByName,
     createClient
 }
 
--- src/controller/productController.js
+++ src/controller/productController.js
@@ -5,7 +5,7 @@ const productModel = require('../model/productModel');
 //Função que executa o metodo da camada model 
 const getAllProducts = (req, res) => {
     const products = productModel.findAll();
-    //se der certo transforme o user em json
+    //se der certo transforme o product em json
     res.status(200).json(products);
 }
 
@@ -32,23 +32,48 @@ const getProductsById = (req, res) => {
     }
 };
 
+// Método do controlador para obter um produto por id
+const getProductByName = (req, res) => {
+
+    // Pegando o id que foi enviado na requisição 
+    // id sendo passado pelo req, parametro que manipula os dados que veio pela requisição
+    // res é a resposta da requisição 
+    // req.params.id | pegue o parametro id 
+    const name = String (req.params.name);
+    
+    //Chamando o método findById do clientModel
+    const product = productModel.findByName(name);
+
+    if (product) {
+        //Responder com status code de 200 (Sucesso!)
+        // e devolver os dados do produto em formato json
+        res.status(200).json(product);
+        //se não tiver sucesso
+    } else {
+        //devolver um 
+        res.status(404).json({ mensagem: 'Produto não encontrado no banco de dados!' });
+    }
+};
+
 //Método do controlador para criar um novo produto
 const createProduct = (req, res) => {
     // Pegando os dados que foram enviador pelo Body da requisição
     //Desestruturação, pegando algo estruturado e guardo em variáveis
     const {name, email, descricao, preco, categoria, estoque} = req.body;
 
-    //validar se foram enviados
+    //validar se foram enviados senão retorna mensagem de erro
     if(!name || !email || !descricao || !preco || !categoria || !estoque) {
         return res.status(400).json({mensagem: "Nome, email, descricao, preco, categoria e estoque são campos obrigatórios!"});
+    
+    //Se estiver tudo correto, retornará a mensagem 201 que é de sucesso
     } else {
-        const newProduct = userModel.create({name, email, descricao, preco, categoria, estoque});
+        const newProduct = productModel.create({name, email, descricao, preco, categoria, estoque});
         res.status(201).json(newProduct);
     }
 }
 
 
-//exportando o metodo getAllUsers
+//exportando os métodos
 module.exports = {
     getAllProducts,
     getProductsById,
